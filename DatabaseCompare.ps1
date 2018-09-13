7192cc31ab5468a50f14768b2fe4c9feaaf6c79f
Ritwik Dutta <rdutta23@yahoo.com>
2018-09-13 16:46:26 -0700

    Added Reporting DB and Devops Compare
    Added Reporting DB and Devops Compare

---
+++ DatabaseCompare.ps1
@@ -0,0 +1,66 @@
+﻿#Set-ExecutionPolicy RemoteSigned
+
+
+#OriginalDacpac
+$OriginalScript = 'C:\Users\Ritwik\Desktop\DatabaseCompare\OriginalScript.sql'
+
+#Source: Place in test file below, list databases by running select * from sys.databases where database_id > 4
+$DatabaseSourceList = 'C:\Users\Ritwik\Desktop\DatabaseCompare\DatabaseList.txt'
+
+#Destination: Database Files Generate Scripts
+$Filepath='C:\Users\Ritwik\Desktop\DatabaseCompare\scripts' # local directory to save build-scripts to
+
+#Resource: https://www.red-gate.com/simple-talk/sql/database-administration/automated-script-generation-with-powershell-and-smo/
+
+foreach($line in Get-Content $DatabaseSourceList) 
+{
+
+    $DataSource='localhost' # server name and instance
+    $Database=$line # the database to copy from
+    # set "Option Explicit" to catch subtle errors
+    set-psdebug -strict
+    $ErrorActionPreference = "stop" # you can opt to stagger on, bleeding, if an error occurs
+    # Load SMO assembly, and if we're running SQL 2008 DLLs load the SMOExtended and SQLWMIManagement libraries
+    $ms='Microsoft.SqlServer'
+    $v = [System.Reflection.Assembly]::LoadWithPartialName( "$ms.SMO")
+    if ((($v.FullName.Split(','))[1].Split('='))[1].Split('.')[0] -ne '9') {
+    [System.Reflection.Assembly]::LoadWithPartialName("$ms.SMOExtended") | out-null
+       }
+    $My="$ms.Management.Smo" #
+    $s = new-object ("$My.Server") $DataSource
+    if ($s.Version -eq  $null ){Throw "Can't find the instance $Datasource"}
+    $db= $s.Databases[$Database] 
+    if ($db.name -ne $Database){Throw "Can't find the database '$Database' in $Datasource"};
+    $transfer = new-object ("$My.Transfer") $db
+    $CreationScriptOptions = new-object ("$My.ScriptingOptions") 
+    $CreationScriptOptions.ExtendedProperties= $true # yes, we want these
+    $CreationScriptOptions.DRIAll= $true # and all the constraints 
+    $CreationScriptOptions.Indexes= $true # Yup, these would be nice
+    $CreationScriptOptions.Triggers= $true # This should be included when scripting a database
+    $CreationScriptOptions.ScriptBatchTerminator = $true # this only goes to the file
+    $CreationScriptOptions.IncludeHeaders = $false; # of course
+    $CreationScriptOptions.ToFileOnly = $true #no need of string output as well
+    $CreationScriptOptions.IncludeIfNotExists = $true # not necessary but it means the script can be more versatile
+    $CreationScriptOptions.Filename =  "$($FilePath)\$($Database)_Build.sql"; 
+    $NewFilename =  "$($FilePath)\$($Database)_Build2.sql"; 
+
+    $transfer = new-object ("$My.Transfer") $s.Databases[$Database]
+ 
+    $transfer.options=$CreationScriptOptions # tell the transfer object of our preferences
+    $transfer.ScriptTransfer()
+    "Created Database " + $Database
+
+    #Get-Content $CreationScriptOptions.Filename | Where { $_ -notmatch "/" } | Set-Content $NewFilename
+
+
+
+    if(Compare-Object -ReferenceObject $(Get-Content $OriginalScript) -DifferenceObject $(Get-Content $NewFilename))
+        {$Database + " Files are different 'n"}
+    Else 
+        {$Database + "Files are the same"}
+
+
+}
+
+
+
---
+++ ReportingView.sql
@@ -0,0 +1,109 @@
+declare @DatabaseTemplate nvarchar(max) = 'Customer2'
+declare @DatabaseDestination nvarchar(max) = 'ReportingDb'
+declare @TableSchema nvarchar(max)
+declare @TableName nvarchar(max)
+
+declare @DatabaseCurrent nvarchar(max)
+declare @DatabaseCounter int = 1
+
+declare @TableListGenerateCode nvarchar(max) = ''
+declare @TableCounter int = 1
+
+declare @DropView nvarchar(255) = ''
+
+declare @TableSelectFromCurrentDatabaseCode nvarchar(max) = ''
+declare @TableSelectFromAllDatabaseCode nvarchar(max) = ''
+
+declare @ColumnSelectFromCurrentTableCode nvarchar(max) = ''
+
+
+
+IF OBJECT_ID('tempdb..#DatabaseList') IS NOT NULL drop table #DatabaseList
+create table #DatabaseList (DatabaseId int identity(1,1), DatabaseName varchar(255))
+
+
+IF OBJECT_ID('tempdb..#TableList') IS NOT NULL drop table #TableList
+create table #TableList (TableId int identity(1,1), TableSchema varchar(255), TableName varchar(255))
+
+IF OBJECT_ID('tempdb..#ColumnList') IS NOT NULL drop table #ColumnList
+create table #ColumnList (ColumnId int identity(1,1), ColumnName varchar(255))
+
+
+insert into #DatabaseList (databasename)
+select db.name  
+from sys.databases db
+where 
+database_id > 4 
+and db_Name(database_id) <> @DatabaseTemplate and db_Name(database_id) <> 'reportingdb' and db_Name(database_id) <> 'electronics' and db_Name(database_id) <> 'mytestdb'
+and state = 0;
+
+set @TableListGenerateCode=
+'
+Insert into #TableList (TableSchema, TableName)
+SELECT TABLE_SCHEMA, TABLE_NAME
+FROM ' + @DatabaseTemplate + '.INFORMATION_SCHEMA.TABLES
+WHERE TABLE_TYPE = ''BASE TABLE'' AND table_catalog = ' + '''' + @DatabaseTemplate + ''''
+
+exec (@TableListGenerateCode)
+
+
+
+while @TableCounter <= (select count(*) from #TableList) 
+begin
+
+	set @TableSchema = (select TableSchema from #TableList where Tableid = @TableCounter)
+	set @TableName = (select TableName from #TableList where Tableid = @TableCounter)
+	set @DatabaseCounter = 1
+
+
+	while @DatabaseCounter <= (select count(*) from #DatabaseList)
+	begin
+
+		set @DatabaseCurrent = (select DatabaseName from #DatabaseList where DatabaseId = @DatabaseCounter)
+
+		truncate table #ColumnList
+		set @ColumnSelectFromCurrentTableCode = 'insert into #ColumnList   select STUFF((
+			SELECT '', 
+		'' + QUOTENAME(c.name) 
+			FROM ' + @DatabaseCurrent + '.sys.columns c where c.object_id = object_id(''' +  @DatabaseCurrent + '.' + @TableSchema + '.' + @Tablename +''')
+			FOR XML PATH(''''), TYPE).value(''.'',''nvarchar(max)''),1,2,'''')'
+
+		exec (@ColumnSelectFromCurrentTableCode)
+
+		set @TableSelectFromCurrentDatabaseCode = 
+	'
+	UNION ALL 
+
+	SELECT ' + (select top 1 ColumnName from #ColumnList) +
+			' 
+	FROM ' + @DatabaseCurrent + '.' + @TableSchema + '.' +  @Tablename + CHAR(13)+CHAR(10) + '
+			'
+		set @TableSelectFromAllDatabaseCode = @TableSelectFromCurrentDatabaseCode + @TableSelectFromAllDatabaseCode 
+
+		set @DatabaseCounter = @DatabaseCounter + 1
+
+	end
+
+		set @TableSelectFromAllDatabaseCode = N' EXEC ' + @DatabaseDestination + '.sys.sp_executesql ' + 'N''Create View ' + @Tablename + 'Vw as 
+		' + RIGHT(@TableSelectFromAllDatabaseCode, LEN(@TableSelectFromAllDatabaseCode) - 13)+''''
+			
+
+		PRINT @TableSelectFromAllDatabaseCode
+
+		--PRINT ('Create View ' + @Tablename + 'Vw as 
+		--' + RIGHT(@TableSelectFromAllDatabaseCode, LEN(@TableSelectFromAllDatabaseCode) - 13)+'''');
+				
+		set @DropView = N' EXEC ' + @DatabaseDestination + '.sys.sp_executesql ' + 'N''IF OBJECT_ID(''''' + @TableName + 'Vw'''', ''''V'''') IS NOT NULL   DROP VIEW ' + @Tablename + 'Vw' + ''''
+
+		exec (@DropView)
+		exec (@TableSelectFromAllDatabaseCode)
+		set @TableCounter = @TableCounter + 1
+		
+		set @DropView = ''
+		set @TableSelectFromCurrentDatabaseCode = ''
+		set @TableSelectFromAllDatabaseCode = ''
+
+
+end
+
+
